bd43ecbe97844ac2aa682e173a6eb8cb32ea7423
Ryan Yang <javathon@gmail.com>
2017-09-07 16:30:47 -0700

    literally spent an hour trying to fix a problem and eventually found out that i was just missing a backslash

--- script.js
+++ script.js
@@ -2,70 +2,30 @@
 /*global $, jQuery*/
 /*jslint white: true */
 
+
 $(function(){
     "use strict";
     
-    
     var controller = new ScrollMagic.Controller();
     
+    var profilePhotoTween = TweenMax.to("#profilePhoto", 100, {x: -200});
+    var profileDescriptionTween = TweenMax.to("#profileDescription", 100, {opacity: 1});
+    
     new ScrollMagic.Scene({
-        triggerElement: "#firstTrigger",
-		duration: 400,
-        offset: 650
-	})
+        duration: 400,
+        offset: 100
+    })
     .setPin("#profilePhoto")
-    .setClassToggle("#profileDescription", "opacityToggle" )
-    .addIndicators({name: "400"}) 
-	.addTo(controller);
-    
+    .setTween(profilePhotoTween)
+    .addIndicators({name: "400"})
+    .addTo(controller);
     
     new ScrollMagic.Scene({
-        triggerElement: "#secondTrigger",
 		duration: 250,
         offset: 400
 	})
 	.setPin("#profileDescription")
-
+    .setTween(profileDescriptionTween)
     .addIndicators({name: "250"}) 
 	.addTo(controller);
-    /*
-    var profileDescription = ScrollMagic.Scene({
-        triggerElement: '#profilePhoto',
-		duration: 300
-	})
-	.setPin("#profilePhoto");
-	controller.addScene(profileDescription);
-    
-    var skillSetDescription = ScrollMagic.Scene({
-        triggerElement: '#profilePhoto',
-		duration: 300
-	})
-	.setPin("#skillSetDescription");
-	controller.addScene(skillSetDescription);
-    */
-    /*
-    if(window.pageYOffset < 250){
-        
-        $("#profilePhoto").stop(1000).css({'top': screen.height/2 + window.pageYOffset}).animate({
-            left: (window.pageYOffset + screen.width/2)
-        }, 10);
-        
-        $("#skillSetDescription").css({'opacity': (window.pageYOffset/300 - 0.4 )});
-        
-    }else{
-        
-        $("#profilePhoto").fadeTo(0, 2.5-(window.pageYOffset/300));
-        
-        $("#skillSetDescription").fadeTo(0, 2.5-(window.pageYOffset/300));
-    }
-    
-    --------------------------------------
-    
-    if(window.pageYOffset < 500){
-        $("#skillSetDescription").fadeTo(0, 1.5-(window.pageYOffset/500));
-    }else{
-        $("#skillSetDescription").fadeTo(0, 500-(window.pageYOffset/500));
-    }
-    */
-    
 });
