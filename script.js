0dff7c688021e175670b65ab14fbd5b46b3f3def
Qiurui Yang <qyang20@go.pasadena.edu>
2017-09-09 15:38:50 -0700

    changed the website to have a more friendly UX and less testy looking

--- script.js
+++ script.js
@@ -2,37 +2,28 @@
 /*global $, jQuery*/
 /*jslint white: true */
 
-
 $(function(){
     "use strict";
     
     var controller = new ScrollMagic.Controller();
     
     var profilePhotoTween = TweenMax.to("#profilePhoto", 100, {x: -200});
-    var profileDescriptionTween = TweenMax.to("#profileDescription", 100, {opacity: 1});
-    var skillSetDescriptionTween = TweenMax.to("#skillSetDescription", 100, {opacity: 1});
+    var profileDescriptionTween = TweenMax.to("#profileDescription", 100, {x: 50, opacity: 1});
+    //var skillSetDescriptionTween = TweenMax.to("#skillSetDescription", 100);
+    //var contactMeLinksTween = TweenMax.to("#contactMeLinks", 100, {opacity: 1});
     
     new ScrollMagic.Scene({
-        duration: 400,
-        offset: 0
+        duration: "40%"
     })
     .setPin("#profilePhoto")
     .setTween(profilePhotoTween)
     .addTo(controller);
     
     new ScrollMagic.Scene({
-		duration: 225,
-        offset: 175
+		duration: "40%",
 	})
 	.setPin("#profileDescription")
     .setTween(profileDescriptionTween)
 	.addTo(controller);
     
-    new ScrollMagic.Scene({
-		duration: 225,
-        offset: 700
-	})
-	.setPin("#skillSetDescription")
-    .setTween(skillSetDescriptionTween)
-	.addTo(controller);
 });
